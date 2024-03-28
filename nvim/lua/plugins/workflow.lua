4b1d223c1cc33b1de3ae51faa0d0a55633f3c6e6
edouardmichelin <edouard.michelin15@gmail.com>
2024-03-28 13:37:30 +0100

    Initial commit

---
+++ nvim/lua/plugins/workflow.lua
@@ -0,0 +1,9 @@
+return {
+   {
+      "fedepujol/move.nvim",
+      keys = {
+         { "K", ":MoveBlock(-1)<CR>", mode = "v" },
+         { "J", ":MoveBlock(1)<CR>", mode = "v" },
+      },
+   },
+}
