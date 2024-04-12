191e674d34d7f070e237fb79a92bd78e7700b9a1
edouardmichelin <edouard.michelin15@gmail.com>
2024-04-12 19:38:53 +0200

    Update spicetify config

--- spicetify/Extracted/Raw/xpui/xpui-routes-lyrics.js
+++ spicetify/Extracted/Raw/xpui/xpui-routes-lyrics.js
@@ -1,1 +1,1 @@
-"use strict";(("undefined"!=typeof self?self:global).webpackChunkopen=("undefined"!=typeof self?self:global).webpackChunkopen||[]).push([[6025],{56568:(e,l,f)=>{f.r(l),f.d(l,{default:()=>n.Z});var n=f(37959)}}]);
+"use strict";(("undefined"!=typeof self?self:global).webpackChunkclient_web=("undefined"!=typeof self?self:global).webpackChunkclient_web||[]).push([[6025],{52687:(e,l,f)=>{f.r(l),f.d(l,{default:()=>n.Z});var n=f(34924)}}]);
