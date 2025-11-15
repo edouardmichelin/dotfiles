69f64a5456eb4d10011e0d34b8393f0b3d99f86b
edouardmichelin <edouard.michelin15@gmail.com>
2025-11-15 02:39:24 +0100

    Remove spicetify extracted files

--- spicetify/Extracted/Raw/xpui/xpui-routes-health-check.js
+++ spicetify/Extracted/Raw/xpui/xpui-routes-health-check.js
@@ -1,2 +1,2 @@
-"use strict";(("undefined"!=typeof self?self:global).webpackChunkclient_web=("undefined"!=typeof self?self:global).webpackChunkclient_web||[]).push([[8212],{61495:(e,l,s)=>{s.r(l),s.d(l,{HealthCheck:()=>n,default:()=>f});var c=s(13274);const n=()=>(0,c.jsx)("h1",{children:"Success!"}),f=n}}]);
+"use strict";(("undefined"!=typeof self?self:global).webpackChunkclient_web=("undefined"!=typeof self?self:global).webpackChunkclient_web||[]).push([[8212],{13648:(e,l,s)=>{s.r(l),s.d(l,{default:()=>c});var n=s(86070);const c=()=>(0,n.jsx)("h1",{children:"Success!"})}}]);
 //# sourceMappingURL=xpui-routes-health-check.js.map
