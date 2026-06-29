1978b230c94fb5dfdefd65c6ab641c071bb96cad
edouardmichelin <edouard.michelin15@gmail.com>
2026-06-29 19:28:50 +0200

    fix(nvim): fix nvim-surround config

--- nvim/lua/plugins/autoclose.lua
+++ nvim/lua/plugins/autoclose.lua
@@ -30,4 +30,34 @@ return {
          },
       },
    },
+   {
+     "kylechui/nvim-surround",
+     version = "^4.0.0",
+     event = "VeryLazy",
+
+     init = function()
+       vim.g.nvim_surround_no_normal_mappings = true
+     end,
+
+     config = function()
+       require("nvim-surround").setup({})
+
+       vim.keymap.set(
+         "n",
+         "gs",
+         "<Plug>(nvim-surround-normal)",
+         { desc = "Add surround" }
+       )
+
+       vim.keymap.set(
+         "n",
+         "gss",
+         "<Plug>(nvim-surround-normal-cur)",
+         { desc = "Surround line" }
+       )
+
+       vim.keymap.set("v", "gs", "<Plug>(nvim-surround-visual)")
+       vim.keymap.set("v", "gS", "<Plug>(nvim-surround-visual-line)")
+     end,
+   },
 }
